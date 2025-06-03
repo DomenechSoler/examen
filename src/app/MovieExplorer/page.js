d55bf9964a311bdd83a1527b6aced2c1b741cfda
DomenechSoler <113708519+DomenechSoler@users.noreply.github.com>
2025-06-03 16:40:31 +0000

    Vistes i navegació

---
+++ src/app/MovieExplorer/page.js
@@ -0,0 +1,7 @@
+export default function MovieExplorerPage() {
+  return (
+    <section className="max-w-2xl mx-auto mt-10 p-6 bg-white rounded shadow">
+      <h2 className="text-3xl font-bold mb-4 text-black">Movie Explorer</h2>
+    </section>
+  );
+}
--- src/miscomponentes/header.js
+++ src/miscomponentes/header.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Link from 'next/link';
 
 function Header() {
     return (
-        <header>
-            <h1>Kilian Domenech</h1>
+        <header className="bg-gray-900 text-white py-4 px-8 flex items-center justify-between">
+           <Link href="/" className="hover:text-yellow-400 transition-colors"><h1 className="text-2xl font-bold">Kilian Domenech</h1></Link> 
+            <nav>
+                <ul className="flex space-x-6">
+                    <li>
+                        <Link href="/About" className="hover:text-yellow-400 transition-colors">About</Link>
+                    </li>
+                    <li>
+                        <Link href="/MovieExplorer" className="hover:text-yellow-400 transition-colors">MovieExplorer</Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
     );
 }
